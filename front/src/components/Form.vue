00910ba64de1d1bd22bd4b867b8fa7b18e8db309
alexperso342 <alexperso342@gmail.com>
2018-11-04 23:54:46 +0100

    projet Maven multi-modules
    Projet Maven / Spring Boot contenant un module back (Hibernate) et un module front (Vue.js). Le back ne contient qu'une entité Vote, et le front une simple page d'accueil. Le tout se compile en jar exécutable (dans back/target) avec un serveur Tomcat embarqué grâce à Spring Boot (exécuter le jar pour démarrer le serveur), et est accessible sur l'url local http://localhost:8088. Hibernate est connecté à une base locale MySQL "catmash". Les données créées ne persistent que pendant le temps de  l’exécution (config à modifier). Le back expose une ressource REST /vote, rendant possible la création d'un vote (POST) et la consultation de tous les votes existants (GET) (url : http://localhost:8088/vote).

---
+++ front/src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="form">
+    <h1>Welcome to catmash!</h1>
+    <h2>Vote for the cutest kitten</h2>
+    <ul id="cats">
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  	name: 'Form',
+	data: {
+	    cats: [] // initialize empty array
+	  },
+	  mounted() { // when the Vue app is booted up, this is run automatically.
+	    var self = this; // create a closure to access component in the callback below
+	    $.getJSON('https://latelier.co/data/cats.json', function(data) {
+	      self.cats = data;
+	    });
+
+	    for(key in Object.keys(cats))
+			$("#cats").append("<li>" + JSON.stringify(key) + "</li>");
+
+  	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
